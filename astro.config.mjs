655721a3b114f34427bdb39119a8efb9eff8c0b3
Lapsar Vitaliy <lops@list.ru>
2026-07-24 15:35:12 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 // import node from "@astrojs/node";
 
 import tailwindcss from "@tailwindcss/vite";
-
-// import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -42,41 +40,4 @@ export default defineConfig({
     enabled: false,
   },
 
-  fonts: [
-    {
-      provider: fontProviders.google(),
-      name: "Mulish",
-      cssVariable: "--site-font-base",
-      subsets: ["latin", "cyrillic"],
-      weights: [400, 500, 600],
-      styles: ["normal"],
-      fallbacks: [
-        "-apple-system",
-        "BlinkMacSystemFont",
-        "Segoe UI",
-        "Roboto",
-        "Helvetica Neue",
-        "Arial",
-        "sans-serif",
-      ],
-    },
-
-    {
-      provider: fontProviders.google(),
-      name: "Manrope",
-      cssVariable: "--site-font-heading",
-      subsets: ["latin", "cyrillic"],
-      weights: [500, 600, 700],
-      styles: ["normal"],
-      fallbacks: [
-        "-apple-system",
-        "BlinkMacSystemFont",
-        "Segoe UI",
-        "Roboto",
-        "Helvetica Neue",
-        "Arial",
-        "sans-serif",
-      ],
-    },
-  ],
 });
